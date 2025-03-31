f8def3b54cf800a9153d9cf57804706ebc8c1094
Rubén Notario <ruben.notario.a@gmail.com>
2025-03-31 12:48:24 +0200

    Assembly Definition Added

--- Runtime/ServerApp.cs
+++ Runtime/ServerApp.cs
@@ -4,7 +4,6 @@ using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEngine.GraphicsBuffer;
 
 
 public class ServerApp : MonoBehaviour
